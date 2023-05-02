1018909dbf1e38dd4b43ed97a258dd5b5f7a60fb
Dar0n <Dar0n@DESKTOP-A3H482S>
2023-05-02 10:12:33 +0300

    Добавьте файлы проекта.

---
+++ )nyame/Chat.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _nyame
+{
+    public partial class Chat : Window
+    {
+        public Chat()
+        {
+            InitializeComponent();
+        }
+
+        private void autorization_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ConectChat connect = new ConectChat();
+                connect.Show();
+            }
+            catch { MessageBox.Show("Error"); }
+        }
+
+        private void new_chat_Click(object sender, RoutedEventArgs e)
+        {
+            chatik.Content = new serv();
+        }
+    }
+}
---
+++ )nyame/serv.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _nyame
+{
+    /// <summary>
+    /// Логика взаимодействия для serv.xaml
+    /// </summary>
+    public partial class serv : Page
+    {
+        private Socket socket;
+        private List<Socket> clients = new List<Socket>();
+        public serv()
+        {
+            InitializeComponent();
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(1000);
+            ListenToClients();
+        }
+        private async Task ListenToClients()
+        {
+            while (true)
+            {
+                var client = await socket.AcceptAsync();
+                clients.Add(client);
+                RecieveMessage(client);
+            }
+        }
+        private async Task RecieveMessage(Socket client)
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await socket.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                catic.Items.Add($"Сообщение от {client.RemoteEndPoint}]: {message}");
+                foreach (var item in clients)
+                {
+                    SendMessage(item, message);
+                }
+            }
+        }
+        private async Task SendMessage(Socket client, string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+        }
+        private void ent_mes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
